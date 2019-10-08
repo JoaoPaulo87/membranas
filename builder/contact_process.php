f49cc1d633c06db4efb9d48f4afb4640cbdeee69
Joao Paulo Correa Parise <correaparisejoaopaulo@gmail.com>
2019-10-08 12:13:12 -0300

    Agrege seccion sobre nosotros en las demas paginas para que vaya a la seccion del video del index

--- builder/contact_process.php
+++ builder/contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "elponyde9@gmail.com";
+    $to = "membranasmardelplata@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
